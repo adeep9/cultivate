1e06bdee68092cd59bde238349cf17acbb28a38c
coreynolastname <corey.berther@gmail.com>
2024-09-26 12:54:55 +1000

    Meta data and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,49 @@ import "./styles/globals.css";
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Cultivate",
-  description: "by Adeep & Corey",
+  title: "Polinate",
+  description: "Blossoming relationship between vendors and suppliers",
+  icons: {
+    icon: "/logosm.svg", // Path relative to the public directory
+    apple: "/logosm.svg", // Apple Touch Icon
+  },
+  openGraph: {
+    title: "Polinate",
+    description: "Blossoming relationship between vendors and suppliers",
+    url: "https://berther.com.au",
+    siteName: "Polinate",
+    images: [
+      {
+        url: "/logosm.svg", // Ensure this is in your public folder
+        width: 1200,
+        height: 630,
+        alt: "Polinate logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@polinate",
+    creator: "@adeep",
+    title: "Polinate",
+    description: "Blossoming relationship between vendors and suppliers",
+    images: "/logosm.svg", // Path for Twitter card image
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  themeColor: "#ffffff", // Match to the primary color of your brand/site
+  alternates: {
+    canonical: "https://berther.com.au",
+  },
 };
+
 
 export default function RootLayout({
   children,
